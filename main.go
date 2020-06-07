39ad5b95b3a743e24cfe9aa362d961f86a1666e0
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-07 16:17:01 +0530

    Add support for simple commands like echo, cat, etc.

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("I'm sorry, what?")
+	}
 }
