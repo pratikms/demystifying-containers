73439f8205cf5925f13ff9489a77f9b0f71c9f79
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-06-07 18:18:46 +0530

    Add PID namespace

--- main.go
+++ main.go
@@ -14,16 +14,25 @@ func must(err error) {
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	must(cmd.Run())
 }
@@ -32,6 +41,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("I'm sorry, what?")
 	}
